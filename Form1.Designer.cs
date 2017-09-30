423a9dd3271806be36a212f1f0e25646f497aa7e
Damin Lee <leesak2003@naver.com>
2017-09-30 12:53:11 +0900

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace GUItest_0811
+{
+    partial class MatchTemplate
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.imageBox3 = new Emgu.CV.UI.ImageBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.imageBox1.Location = new System.Drawing.Point(12, 85);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(250, 200);
+            this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(276, 83);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(250, 200);
+            this.imageBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox2.TabIndex = 3;
+            this.imageBox2.TabStop = false;
+            // 
+            // imageBox3
+            // 
+            this.imageBox3.Location = new System.Drawing.Point(534, 83);
+            this.imageBox3.Name = "imageBox3";
+            this.imageBox3.Size = new System.Drawing.Size(250, 200);
+            this.imageBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox3.TabIndex = 4;
+            this.imageBox3.TabStop = false;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox1.Location = new System.Drawing.Point(12, 291);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(250, 120);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox2.Location = new System.Drawing.Point(276, 289);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(250, 120);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox3.Location = new System.Drawing.Point(534, 291);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(250, 120);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(390, 1);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(394, 54);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(-1, 1);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(394, 54);
+            this.textBox5.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(89, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 16);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Camera 1";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(354, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 16);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Camera 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(617, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 16);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Camera 3";
+            // 
+            // MatchTemplate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 411);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.imageBox3);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "MatchTemplate";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private Emgu.CV.UI.ImageBox imageBox3;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Emgu.CV;
+using Emgu.CV.UI;
+using Emgu.CV.Util;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Cuda;
+
+namespace GUItest_0811
+{
+    public partial class MatchTemplate : Form
+    {
+        public MatchTemplate()
+        {
+            InitializeComponent();
+        }
+
+
+        Stopwatch GPUtime1 = new Stopwatch();
+        Stopwatch GPUtime2 = new Stopwatch();
+        Stopwatch GPUtime3 = new Stopwatch();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            Image<Bgr, byte> sourceimage = new Image<Bgr, byte>(@"C:\\MatchSource.jpg");
+            Image<Bgr, byte> template = new Image<Bgr, byte>(@"C:\\Template.jpg");
+            Image<Bgr, byte> imagetoshow = sourceimage.Copy();
+
+            
+           
+
+            GpuMat gsourceimage = new GpuMat(sourceimage);
+            GpuMat gtemplate = new GpuMat(template);
+            GpuMat gimagetoshow = new GpuMat(imagetoshow);
+
+            GPUtime1.Start();
+
+            double gpuminval = 0, gpumaxval = 0;
+            Point gpuminloc = Point.Empty, gpumaxloc = Point.Empty;
+
+            using (CudaTemplateMatching buffer = new CudaTemplateMatching(DepthType.Cv8U, 3, TemplateMatchingType.CcorrNormed))
+
+                buffer.Match(gsourceimage, gtemplate, gimagetoshow);
+
+            CudaInvoke.MinMaxLoc(gimagetoshow, ref gpuminval, ref gpumaxval, ref gpuminloc, ref gpumaxloc, null);
+
+            Console.WriteLine("minval:" + gpuminval + "maxval:" + gpumaxval + "maxloc:(" + gpumaxloc.X + "," + gpumaxloc.Y + ")" + "minloc:(" + gpuminloc.X + "," + gpuminloc.Y + ")");
+
+            GPUtime1.Stop();
+
+            if (gpumaxval >0.9)
+            {
+                Rectangle match = new Rectangle(gpumaxloc, template.Size);
+
+                imagetoshow.Draw(match, new Bgr(Color.Red), 15);
+
+                imageBox1.Image = imagetoshow;
+
+                textBox1.Text ="Success" + Environment.NewLine + "정확도:" +gpumaxval.ToString("0.###")
+                                + Environment.NewLine + "소요시간:"+ GPUtime1.ElapsedMilliseconds.ToString() + "ms";
+                
+            }
+            else
+            {
+                textBox1.AppendText("Fail");
+            }
+            
+            imagetoshow.Dispose();
+            template.Dispose();
+            gtemplate.Dispose();
+            gimagetoshow.Dispose();
+           
+            
+            //try
+            //{
+            //CvInvoke.Imshow("test", imagetoshow);
+            //}
+            //catch(Exception ex)
+            //{
+            //    Console.WriteLine(ex);
+            //}
+
+            ////////////////////////////2nd Camera////////////////////////////////////
+
+            template = new Image<Bgr, byte>(@"C:\\Template.jpg");
+            imagetoshow = sourceimage.Copy();
+
+            gtemplate = new GpuMat(template);
+            gimagetoshow = new GpuMat(imagetoshow);
+
+            GPUtime2.Start();
+
+            gpuminval = 0;
+            gpumaxval = 0;
+            gpuminloc = Point.Empty;
+            gpumaxloc = Point.Empty;
+
+            using (CudaTemplateMatching buffer = new CudaTemplateMatching(DepthType.Cv8U, 3, TemplateMatchingType.CcorrNormed))
+
+                buffer.Match(gsourceimage, gtemplate, gimagetoshow);
+
+            CudaInvoke.MinMaxLoc(gimagetoshow, ref gpuminval, ref gpumaxval, ref gpuminloc, ref gpumaxloc, null);
+
+            Console.WriteLine("minval:" + gpuminval + "maxval:" + gpumaxval + "maxloc:(" + gpumaxloc.X + "," + gpumaxloc.Y + ")" + "minloc:(" + gpuminloc.X + "," + gpuminloc.Y + ")");
+
+            GPUtime2.Stop();
+
+            if (gpumaxval > 0.9)
+            {
+                Rectangle match = new Rectangle(gpumaxloc, template.Size);
+
+                imagetoshow.Draw(match, new Bgr(Color.Red), 15);
+
+                imageBox2.Image = imagetoshow;
+
+                textBox2.Text = "Success" + Environment.NewLine + "정확도:"+ gpumaxval.ToString("0.###")
+                                + Environment.NewLine + "소요시간:"+ GPUtime2.ElapsedMilliseconds.ToString() + "ms";
+
+            }
+            else
+            {
+                textBox2.AppendText("Fail");
+            }
+            //CvInvoke.cvReleaseImage(ref imagetoshow);
+
+            imagetoshow.Dispose();
+            template.Dispose();
+            gtemplate.Dispose();
+            gimagetoshow.Dispose();
+
+            ////////////////////////////////////3rd Camera///////////////////////////////////
+
+            template = new Image<Bgr, byte>(@"C:\\Template.jpg");
+            imagetoshow = sourceimage.Copy();
+
+            gtemplate = new GpuMat(template);
+            gimagetoshow = new GpuMat(imagetoshow);
+
+            GPUtime3.Start();
+
+            gpuminval = 0;
+            gpumaxval = 0;
+            gpuminloc = Point.Empty;
+            gpumaxloc = Point.Empty;
+
+            using (CudaTemplateMatching buffer = new CudaTemplateMatching(DepthType.Cv8U, 3, TemplateMatchingType.CcoeffNormed))
+
+                buffer.Match(gsourceimage, gtemplate, gimagetoshow);
+
+            CudaInvoke.MinMaxLoc(gimagetoshow, ref gpuminval, ref gpumaxval, ref gpuminloc, ref gpumaxloc, null);
+
+            Console.WriteLine("minval:" + gpuminval + "maxval:" + gpumaxval + "maxloc:(" + gpumaxloc.X + "," + gpumaxloc.Y + ")" + "minloc:(" + gpuminloc.X + "," + gpuminloc.Y + ")");
+
+            GPUtime3.Stop();
+
+            if (gpumaxval > 0.9)
+            {
+                Rectangle match = new Rectangle(gpumaxloc, template.Size);
+
+                imagetoshow.Draw(match, new Bgr(Color.Red), 15);
+
+                imageBox3.Image = imagetoshow;
+
+                textBox3.Text = "Success" + Environment.NewLine + "정확도:"+ gpumaxval.ToString("0.###")
+                                + Environment.NewLine +"소요시간:"+ GPUtime2.ElapsedMilliseconds.ToString() + "ms";
+
+            }
+            else
+            {
+                textBox3.AppendText("Fail");
+            }
+            //CvInvoke.cvReleaseImage(ref imagetoshow);
+
+            imagetoshow.Dispose();
+            template.Dispose();
+            gtemplate.Dispose();
+            gimagetoshow.Dispose();
+
+
+            //int i = 0;
+            //Image<Bgr, byte>[] ttt = new Image<Bgr, byte>[3];
+            //Mat[] ww = new Mat[5];
+
+            //for(i = 0; i<3; i++)
+            //{ 
+            //ttt[i] =[@"C:\\template2.jpg", @"C:\\template3.jpg", @"C:\\template4.jpg"];
+            //ww[i] = [1, 2, 3, 4, 5];
+            //}
+
+
+        }
+
+        private void splitter1_SplitterMoved(object sender, SplitterEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
